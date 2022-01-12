b5aaf496f339ab489e2a77aa6c212eac9f11b174
Samieh Abdeljabbar <abdels6375@gmail.com>
2022-01-12 12:21:33 -0600

    identifiers

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ print(area(3))
 # Elements: Objects, Identifiers, Operators, Delimiters, Keywords, Comments, Blank Lines, White Space
 # Objects in this code are: 4, 5, [6, 7], 6, 7, "Hello", "Hi", Function definition, 2,3,9
 #Idenitifiers in this code are: a, b, c, d, area, c
-
+# So for Example, my name would be the Identifier (samieh) and the object would be me
